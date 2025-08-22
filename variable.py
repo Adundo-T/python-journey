93f5eb281d5b4726e1a1d02415f2d0cf3f2a79cc
Adundo-T <adundotala@gmail.com>
2025-08-22 16:33:45 +0300

    what i learnt today

---
+++ variable.py
@@ -0,0 +1,18 @@
+x = 3
+y = "Boma"
+print(x)
+print(y)
+# Variable names are case-sensitive.
+   
+x = 7       # x is of type int
+x = "Auma"  # x is now of type str
+print(x)
+
+x = 5
+y = "Boma"
+print(type(x))
+print(type(y))
+
+x = "Auma"
+# is the same as
+x = 'Auma'
